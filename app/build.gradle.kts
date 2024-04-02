25876d949608c1fbc935502f1c6ac6393ee694cd
ShaiNachum <100000990+ShaiNachum@users.noreply.github.com>
2024-04-02 17:56:24 +0300

    first

---
+++ app/build.gradle.kts
@@ -0,0 +1,52 @@
+plugins {
+    alias(libs.plugins.androidApplication)
+    id("com.google.gms.google-services")
+}
+
+android {
+    namespace = "com.example.myRecipes"
+    compileSdk = 34
+
+    defaultConfig {
+        applicationId = "com.example.myRecipes"
+        minSdk = 26
+        targetSdk = 34
+        versionCode = 1
+        versionName = "1.0"
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
+    }
+}
+
+dependencies {
+
+    implementation(libs.appcompat)
+    implementation(libs.material)
+    implementation(libs.activity)
+    implementation(libs.constraintlayout)
+    testImplementation(libs.junit)
+    androidTestImplementation(libs.ext.junit)
+    androidTestImplementation(libs.espresso.core)
+
+    //Authentication
+    implementation(libs.firebase.analytics)
+    implementation(platform(libs.firebase.bom))
+
+    implementation(libs.firebase.authentication.ui)
+
+    implementation(libs.gms.play.services.auth)
+}
---
+++ app/src/main/java/com/example/myrecipes/Models/Recipe.java
@@ -0,0 +1,4 @@
+package com.example.myrecipes.Models;
+
+public class Recipe {
+}
